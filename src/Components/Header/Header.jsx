0c05655882f86f076ea382edf294d28a1fc9a10c
Mahadi Hasan <mahadihasan8461@gmail.com>
2023-04-08 22:46:45 +0600

    ract router added

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import './Header.css'
-import logo from '../../assets/images/Logo.svg'
+import React from "react";
+import "./Header.css";
+import logo from "../../assets/images/Logo.svg";
 const Header = () => {
-    return (
-        <nav className='header'>
-            <img src={logo} alt="" />
-            <div className='nav-link'>
-            <a href="/shop">Shop</a>
-            <a href="/order">Order</a>
-            <a href="/inventory">Inventory</a>
-            <a href="/login">Login</a>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="header">
+      <img src={logo} alt="" />
+      <div className="nav-link">
+        <a href="/">Shop</a>
+        <a href="/orders">Order</a>
+        <a href="/inventory">Inventory</a>
+        <a href="/login">Login</a>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
---
+++ src/Components/Inventory/Inventory.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Inventory = () => {
+  return <div>This is invertory page</div>;
+};
+
+export default Inventory;
